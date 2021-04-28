8910d13f3beb7140b43a2db37167565ada02d359
Michael-saeek <saeeksitow@gmail.com>
2021-04-28 19:43:48 -0300

    Responsive Header finished

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-<<<<<<< HEAD
 window.addEventListener("scroll", function() {
-    const header = document.querySelector('div#header');
+    const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
-    })
-=======
-function hola() {
-    console.log('hola')
-}
 
-hola()
->>>>>>> 92cff598e91d39b5666a9738e564a83885bef7c5
+});
+
+const menunav = document.querySelector('nav')
+document.querySelector('.toggle').onclick = function() {
+    this.classList.toggle('active');
+    menunav.classList.toggle('active');
+}
